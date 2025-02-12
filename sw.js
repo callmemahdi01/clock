a4e72256d0a543803f514f038f013b29c15f0f5f
callmemahdi01 <mr1451493@gmail.com>
2025-02-12 12:58:35 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE_NAME = 'clock-cache-v1.3';
+const CACHE_NAME = 'clock-cache-v1.4';
 const urlsToCache = [
   '/clock/',
   '/clock/index.html',
   '/clock/manifest.json',
-  '/clock/icon192×192.png',
-  '/clock/icon512×512.png',
+  '/clock/icon192x192.png',
+  '/clock/icon512x512.png',
   '/clock/sw.js'
 ].map(url => new Request(url, { credentials: 'same-origin' }));
 
-// استراتژی cacheFirst: ابتدا از کش می‌خوانیم، در صورت عدم وجود، به شبکه مراجعه می‌کنیم
 async function cacheFirst(request) {
   const cachedResponse = await caches.match(request);
   return cachedResponse || networkFirst(request);
 }
 
-// استراتژی networkFirst: ابتدا تلاش می‌کنیم از شبکه بخوانیم، اگر موفق بود، نتیجه در کش ذخیره شده و بازگردانده می‌شود؛ در غیر این صورت، از کش استفاده می‌کنیم
 async function networkFirst(request) {
   try {
     const response = await fetch(request);
-    // بررسی می‌کنیم که پاسخ معتبر است
     if (response && response.status === 200 && response.type === 'basic') {
       const cache = await caches.open(CACHE_NAME);
       cache.put(request, response.clone());
@@ -29,20 +26,22 @@ async function networkFirst(request) {
   }
 }
 
-// رویداد نصب (install): در زمان نصب، فایل‌های مورد نظر را کش می‌کنیم
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // بلافاصله فعال شود
   );
 });
 
-// رویداد fetch: درخواست‌ها را مدیریت می‌کنیم و ابتدا استراتژی cacheFirst را اعمال می‌کنیم
 self.addEventListener('fetch', event => {
-  event.respondWith(cacheFirst(event.request));
+  if (event.request.url.includes('index.html')) {
+    event.respondWith(networkFirst(event.request));
+  } else {
+    event.respondWith(cacheFirst(event.request));
+  }
 });
 
-// رویداد activate: در زمان فعال شدن سرویس ورکر، کش‌های قدیمی پاک می‌شوند
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -54,6 +53,6 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim()) // اطمینان از اعمال نسخه‌ی جدید
   );
 });
